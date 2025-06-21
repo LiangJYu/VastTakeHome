920b61a53fdf3ec5fd062d210b9046004c457b6b
Liang Yu <liangjyu@gmail.com>
2025-06-21 10:16:35 -0700

    time constants as static class members instead of enums

--- src/Constants.h
+++ src/Constants.h
@@ -7,32 +7,33 @@ namespace MiningSimulation {
  * These constants define default timing values used by the OperationClock
  * class. All values are in seconds.
  */
-enum class TimeConstants : unsigned int
-{
+struct TimeConstants {
     // Default duration (72 hours in seconds)
-    default_stop_time = 72 * 3600,
+    static constexpr unsigned int default_stop_time = 72 * 3600;
     // Default increment step (1 second)
-    default_increment_time = 1,
+    static constexpr unsigned int default_increment_time = 1;
     //
-    transit_time = 30 * 60,
+    static constexpr unsigned int transit_time = 30 * 60;
     //
-    unload_time = 5 * 60
+    static constexpr unsigned int unload_time = 5 * 60;
+    //
+    static constexpr unsigned int min_mining_time = 3600;
+    //
+    static constexpr unsigned int mining_time_range = 4 * 3600;
 };
 
 enum class TruckState {
     transit_to_mine,
     mining,
     transit_to_unload,
-    waiting,
-    unloading
+    station_processing
 };
 
 enum class TruckEvent {
     mine_arrival,
     mining_complete,
     station_arrival,
-    waiting_complete,
-    unload_complete
+    processing_complete
 };
 
 }
