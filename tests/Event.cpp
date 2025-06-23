7feebc24c2232d19e065f65d339896e5a7e88c56
Liang Yu <liangjyu@gmail.com>
2025-06-23 02:31:31 -0700

    removed redundant test Event testing

--- tests/Event.cpp
+++ tests/Event.cpp
@@ -3,18 +3,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(EventTest, DefaultInitialization) {
-
-    Event event(0, TruckEvent::mine_arrival, 1);
-
-    EXPECT_EQ(event.get_id(), 0);
-    EXPECT_EQ(event.get_completion_event(), TruckEvent::mine_arrival);
-    EXPECT_EQ(event.get_t_completion(), 1);
-}
-
 TEST(EventTest, GreaterThanTest) {
-    Event event0(0, TruckEvent::mine_arrival, 0);
-    Event event1(1, TruckEvent::mine_arrival, 1);
+    Event event0(0, TruckEvent::mining_trip_complete, 0);
+    Event event1(1, TruckEvent::mining_trip_complete, 1);
 
     EXPECT_TRUE(event1 > event0);
     EXPECT_FALSE(event0 > event1);
